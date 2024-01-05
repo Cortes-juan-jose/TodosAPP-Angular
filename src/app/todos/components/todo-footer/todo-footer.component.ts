053cbc1fb02bc2439f4ab8c90899bfc7a542558f
Cortes-juan-jose <cortes.juan.jose205@gmail.com>
2024-01-05 01:13:28 +0100

    Estilos establecidos a los filtros activos

--- src/app/todos/components/todo-footer/todo-footer.component.ts
+++ src/app/todos/components/todo-footer/todo-footer.component.ts
@@ -11,10 +11,16 @@ import { establecerFiltro, filtrosValidos } from 'src/app/filtro/filtro.actions'
 export class TodoFooterComponent implements OnInit{
 
   tareasPendientes!: number;
+  filtroActivado!: string;
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store.select('filtro').subscribe({
+      next: filtro => this.filtroActivado = filtro,
+      error: err => console.log(err),
+      complete: () => console.log('Completado'),
+    })
     this.tareasPendientes;
   }
 
